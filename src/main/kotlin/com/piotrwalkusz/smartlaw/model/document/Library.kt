22562d97cf59d47a7a0e3d1d313a6509450dccf8
Piotr Walkusz <piotrwalkusz1@gmail.com>
2020-09-14 21:00:56 +0200

    Update

--- src/main/kotlin/com/piotrwalkusz/smartlaw/model/document/Library.kt
+++ src/main/kotlin/com/piotrwalkusz/smartlaw/model/document/Library.kt
@@ -1,5 +1,6 @@
 package com.piotrwalkusz.smartlaw.model.document
 
+import com.piotrwalkusz.smartlaw.model.annotation.AnnotationType
 import com.piotrwalkusz.smartlaw.model.common.Id
 import com.piotrwalkusz.smartlaw.model.element.Element
 import com.piotrwalkusz.smartlaw.model.rule.Rule
@@ -9,5 +10,6 @@ data class Library(
         val name: String,
         val description: String? = null,
         var rules: List<Rule> = listOf(),
-        var elements: List<Element> = listOf()
+        var elements: List<Element> = listOf(),
+        var annotations: List<AnnotationType> = listOf()
 ) : Document
